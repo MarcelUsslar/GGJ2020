62961eb34310e746e506db0cbcb4b3114aab51bf
Marcel Usslar <marcel.usslar@kolibrigames.com>
2020-02-01 12:44:01 +0100

    Added button interaction script.

---
+++ Assets/Scripts/ButtonInteraction.cs
@@ -0,0 +1,18 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonInteraction : MonoBehaviour
+{
+    [SerializeField] private Button _button;
+
+    private void Start()
+    {
+        _button.OnClickAsObservable().Subscribe(_ => ExecuteMethod()).AddTo(gameObject);
+    }
+
+    private void ExecuteMethod()
+    {
+        Debug.Log("Button was clicked");
+    }
+}
