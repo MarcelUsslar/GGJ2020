daa28d6d6d2b4719283b0aff750b7b04e3869b9c
Marcel Usslar <marcel.usslar@kolibrigames.com>
2020-02-01 18:05:51 +0100

    Moved IInputDetection into GameUtility class.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using JetBrains.Annotations;
 using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
@@ -36,17 +35,17 @@ public class GameController : MonoBehaviour
 
         var inputDetectionTrigger = _config.Trigger(_eventIndex);
         
-        Log(_debugText, "Create Input Detection for {0}", inputDetectionTrigger);
+        GameUtility.Log(_debugText, "Create Input Detection for {0}", inputDetectionTrigger);
 
         var eventPrefab = Instantiate(_config.Prefab(_eventIndex), transform);
-        var inputDetection = CreateInputDetection(inputDetectionTrigger, eventPrefab);
+        var inputDetection = GameUtility.CreateInputDetection(inputDetectionTrigger, eventPrefab, _stateDisposable, _stateText, _config);
 
         _detectionDisposable.Disposable = inputDetection.Triggered
             .Subscribe(trigger =>
             {
                 Destroy(eventPrefab);
 
-                Log(_debugText, "{0} condition met.", inputDetectionTrigger);
+                GameUtility.Log(_debugText, "{0} condition met.", inputDetectionTrigger);
 
                 _delayDisposable.Disposable = Observable.Timer(TimeSpan.FromSeconds(_delay))
                     .Subscribe(delay =>
@@ -55,73 +54,5 @@ public class GameController : MonoBehaviour
                         SpawnEvent();
                     });
             });
-    }
-
-    private IInputDetection CreateInputDetection(InputDetection inputDetection, GameObject eventPrefab)
-    {
-        switch (inputDetection)
-        {
-            case InputDetection.Battery:
-                return CreateBatteryDetection();
-            case InputDetection.Internet:
-                return CreateInternetDetection(NetworkReachability.ReachableViaCarrierDataNetwork, NetworkReachability.ReachableViaLocalAreaNetwork);
-            case InputDetection.Wifi:
-                return CreateInternetDetection(NetworkReachability.ReachableViaLocalAreaNetwork);
-            case InputDetection.MobileData:
-                return CreateInternetDetection(NetworkReachability.ReachableViaCarrierDataNetwork);
-            case InputDetection.Disconnected:
-                return CreateInternetDetection(NetworkReachability.NotReachable);
-            case InputDetection.Shake:
-                Log(_stateText, "Waiting for shake");
-                return new ShakeDetection(_config.ShakeThreshold);
-            case InputDetection.ScreenBrightnessUp:
-                return CreateScreenBrightnessDetection(brightness => brightness >= 1.0f);
-            case InputDetection.ScreenBrightnessDown:
-                return CreateScreenBrightnessDetection(brightness => brightness <= 0.5f);
-            case InputDetection.VolumeUp:
-                return CreateVolumeDetection(true);
-            case InputDetection.VolumeDown:
-                return CreateVolumeDetection(false);
-            default:
-                throw new ArgumentOutOfRangeException(nameof(inputDetection), inputDetection, null);
-        }
-    }
-
-    private BatteryDetection CreateBatteryDetection()
-    {
-        var batteryDetection = new BatteryDetection(BatteryStatus.Charging, BatteryStatus.Full);
-        _stateDisposable.Disposable = batteryDetection.BatteryStatus.Subscribe(status => Log(_stateText, "Status: {0}", status));
-
-        return batteryDetection;
-    }
-
-    private InternetDetection CreateInternetDetection(params NetworkReachability[] allowedNetworkStates)
-    {
-        var internetDetection = new InternetDetection(allowedNetworkStates);
-        _stateDisposable.Disposable = internetDetection.InternetState.Subscribe(status => Log(_stateText, "Status: {0}", status));
-
-        return internetDetection;
-    }
-    
-    private ScreenBrightnessDetection CreateScreenBrightnessDetection(Func<float, bool> hasReachedBrightness)
-    {
-        var screenBrightnessDetection = new ScreenBrightnessDetection(hasReachedBrightness);
-        _stateDisposable.Disposable = screenBrightnessDetection.ScreenBrightness.Subscribe(brightness => Log(_stateText, "Status: {0}", brightness));
-        
-        return screenBrightnessDetection;
-    }
-
-    private VolumeDetection CreateVolumeDetection(bool increaseVolume)
-    {
-        var volumeDetection = new VolumeDetection(increaseVolume);
-        _stateDisposable.Disposable = volumeDetection.CurrentVolume.Subscribe(volume => Log(_stateText, "Volume: {0}", volume));
-
-        return volumeDetection;
-    }
-
-    [StringFormatMethod("message")]
-    private static void Log(Text textField, string message, params object[] parameters)
-    {
-        textField.text = string.Format(message, parameters);
     }
 }
---
+++ Assets/Scripts/GameUtility.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using JetBrains.Annotations;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class GameUtility
+{
+    [StringFormatMethod("message")]
+    public static void Log(Text textField, string message, params object[] parameters)
+    {
+        textField.text = string.Format(message, parameters);
+    }
+    public static IInputDetection CreateInputDetection(InputDetection inputDetection, GameObject eventPrefab,
+        SerialDisposable serialDisposable, Text stateText, LoopConfig config)
+    {
+        switch (inputDetection)
+        {
+            case InputDetection.Battery:
+                return CreateBatteryDetection(serialDisposable, stateText);
+            case InputDetection.Internet:
+                return CreateInternetDetection(serialDisposable, stateText, NetworkReachability.ReachableViaCarrierDataNetwork, NetworkReachability.ReachableViaLocalAreaNetwork);
+            case InputDetection.Wifi:
+                return CreateInternetDetection(serialDisposable, stateText, NetworkReachability.ReachableViaLocalAreaNetwork);
+            case InputDetection.MobileData:
+                return CreateInternetDetection(serialDisposable, stateText, NetworkReachability.ReachableViaCarrierDataNetwork);
+            case InputDetection.Disconnected:
+                return CreateInternetDetection(serialDisposable, stateText, NetworkReachability.NotReachable);
+            case InputDetection.Shake:
+                Log(stateText, "Waiting for shake");
+                return new ShakeDetection(config.ShakeThreshold);
+            case InputDetection.ScreenBrightnessUp:
+                return CreateScreenBrightnessDetection(serialDisposable, stateText, brightness => brightness >= 1.0f);
+            case InputDetection.ScreenBrightnessDown:
+                return CreateScreenBrightnessDetection(serialDisposable, stateText, brightness => brightness <= 0.5f);
+            case InputDetection.VolumeUp:
+                return CreateVolumeDetection(serialDisposable, stateText, true);
+            case InputDetection.VolumeDown:
+                return CreateVolumeDetection(serialDisposable, stateText, false);
+            default:
+                throw new ArgumentOutOfRangeException(nameof(inputDetection), inputDetection, null);
+        }
+    }
+
+    private static BatteryDetection CreateBatteryDetection(SerialDisposable serialDisposable, Text stateText)
+    {
+        var batteryDetection = new BatteryDetection(BatteryStatus.Charging, BatteryStatus.Full);
+        serialDisposable.Disposable = batteryDetection.BatteryStatus.Subscribe(status => Log(stateText, "Status: {0}", status));
+
+        return batteryDetection;
+    }
+
+    private static InternetDetection CreateInternetDetection(SerialDisposable serialDisposable, Text stateText,
+        params NetworkReachability[] allowedNetworkStates)
+    {
+        var internetDetection = new InternetDetection(allowedNetworkStates);
+        serialDisposable.Disposable = internetDetection.InternetState.Subscribe(status => Log(stateText, "Status: {0}", status));
+
+        return internetDetection;
+    }
+
+    private static ScreenBrightnessDetection CreateScreenBrightnessDetection(SerialDisposable serialDisposable, Text stateText,
+        Func<float, bool> hasReachedBrightness)
+    {
+        var screenBrightnessDetection = new ScreenBrightnessDetection(hasReachedBrightness);
+        serialDisposable.Disposable = screenBrightnessDetection.ScreenBrightness.Subscribe(brightness => Log(stateText, "Status: {0}", brightness));
+
+        return screenBrightnessDetection;
+    }
+
+    private static VolumeDetection CreateVolumeDetection(SerialDisposable serialDisposable, Text stateText, bool increaseVolume)
+    {
+        var volumeDetection = new VolumeDetection(increaseVolume);
+        serialDisposable.Disposable = volumeDetection.CurrentVolume.Subscribe(volume => Log(stateText, "Volume: {0}", volume));
+
+        return volumeDetection;
+    }
+}
