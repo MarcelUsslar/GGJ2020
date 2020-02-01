05c43dee340b24b050104a76866f45c9b2a68828
Samer <samer@popcore.com>
2020-02-01 16:11:24 +0100

    Added events 1,2,3 & 4

---
+++ Assets/Scripts/Events/EventBlackscreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventBlackscreen : MonoBehaviour
+{
+
+	bool Flash;
+
+    void update()
+	{
+
+		CheckFlash();
+
+	}
+
+    void CheckFlash()
+	{
+
+        if (Flash)
+		{
+
+			Debug.Log("Done");
+
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/Events/EventBluescreen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventBluescreen : MonoBehaviour
+{
+
+    void OnMouseDown()
+	{
+
+
+		Debug.Log("Done");
+
+
+	}
+
+}
